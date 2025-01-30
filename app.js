86c82baa43636100026061820eac3a20e83bd829
awaiskhan20 <awaiskhan.raees123@gmail.com>
2025-01-31 04:32:13 +0500

    Add Todos Work Done

--- app.js
+++ app.js
@@ -20,3 +20,28 @@ logout_btn.addEventListener("click", function () {
         console.log(errorMessage);
     });
 });
+
+
+import { collection, addDoc } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-firestore.js";
+import { db } from "./firebaseconfig.js";
+
+const form = document.querySelector("#form");
+const todo_title = document.querySelector(".todo_title");
+const todo_description = document.querySelector(".todo_description");
+
+form.addEventListener('submit', async (event) => {
+    event.preventDefault();
+    console.log(todo_title.value);
+    console.log(todo_description.value);
+    
+    try {
+        const docRef = await addDoc(collection(db, "todos"), {
+            title: todo_title.value,
+            description: todo_description.value
+        });
+        console.log("Document written with ID: ", docRef.id);
+    } catch (error) {
+        const errorMessage = error.message;
+        console.log(errorMessage);
+    }
+});
--- firebaseconfig.js
+++ firebaseconfig.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-firestore.js";
 
 const firebaseConfig = {
     apiKey: "AIzaSyABxgWkqMRi1oDupWWL0GddORfgqLMyag8",
@@ -13,3 +14,4 @@ const firebaseConfig = {
 
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+export const db = getFirestore(app);
